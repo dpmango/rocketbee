775fb006566552d0743ab39af50cc8856f0d9e99
dpmango <xs290@me.com>
2017-04-01 02:12:26 +0300

    init

---
+++ src/js/main.js
@@ -0,0 +1,115 @@
+'use strict';
+
+$(document).ready(function () {
+
+  var _window = $(window);
+
+  // Prevent # behavior
+  $('[href="#"]').click(function (e) {
+    e.preventDefault();
+  });
+
+  // Smoth scroll
+  $('a[href^="#section"]').click(function (e) {
+    var el = $(this).attr('href');
+    $('body, html').animate({
+      scrollTop: $(el).offset().top }, 1000);
+    return false;
+  });
+
+  // HAMBRUGER
+  $('.js-toggleMobileMenu').on('click', function () {
+    $(this).toggleClass('is-active');
+    $('.navi-mobile').toggleClass('is-active');
+  });
+
+  ///////
+  // UI
+  ///////
+  // -- select
+  $('.ui-select').on('click', function (e) {
+    $(this).toggleClass('active');
+  });
+
+  $('.ui-select__drop span').on('click', function (e) {
+    var currentValue = $(this).data('select');
+    $(this).closest('.ui-select').find('label').text(currentValue);
+    $(this).closest('.ui-select').find('label').addClass('selected');
+    $('.ui-select').removeClass('ui-select--error');
+
+    // paste  value
+    $(this).closest('.ui-select').find('input[type="hidden"]').val(currentValue);
+  });
+
+  $(document).click(function (e) {
+    var _value = $('.ui-select');
+
+    if (!_value.is(e.target) && _value.has(e.target).length === 0) {
+      _value.removeClass('active');
+    }
+  });
+
+  // owl
+  $('#owlFirst').owlCarousel({
+    loop: true,
+    nav: true,
+    margin: 0,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+
+  // Magnific Popup
+  $('.popup-with-zoom-anim').magnificPopup({
+    type: 'inline',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+  });
+
+  $('.popup-with-move-anim').magnificPopup({
+    type: 'inline',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-slide-bottom'
+  });
+
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    }
+  });
+
+  // Masked input
+  $("#date").mask("99/99/9999", { placeholder: "mm/dd/yyyy" });
+  $("input[name='phone']").mask("9 (999) 999-9999");
+  $("#tin").mask("99-9999999");
+  $("#ssn").mask("999-99-9999");
+});
